cac2243bc8416c9f46976bdc2a7eb8b90f27ffd6
cannoli9917 <katie.pennachio@gmail.com>
2019-04-06 09:03:52 -0400

    person gift event serializer change

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,6 +4,10 @@ class UserSerializer < ActiveModel::Serializer
   def events
     self.object.events.map do |event|
         date = Date.strptime(event.date, "%m-%d-%Y")
+
+        pge = event.person_gift_events.map do |pge|
+          {person: pge.person, gift: pge.gift, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
+        end
       {
         id: event.id,
         title: event.title,
@@ -14,8 +18,7 @@ class UserSerializer < ActiveModel::Serializer
         year: date.year,
         notes: event.notes,
         registryLink: event.registry_link,
-        people: event.people,
-        gifts: event.gifts,
+        person_gift_events: pge,
         gift_ideas: event.gift_ideas
       }
     end
@@ -23,12 +26,16 @@ class UserSerializer < ActiveModel::Serializer
 
   def people
     self.object.people.map do |person|
+      pge = person.person_gift_events.map do |pge|
+        {event: pge.event, gift: pge.gift, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
+      end
       {
         id: person.id,
         name: person.name,
         notes: person.notes,
         gifts: person.gifts,
         events: person.events,
+        person_gift_events: pge,
         gift_ideas: person.gift_ideas
       }
     end
@@ -36,6 +43,9 @@ class UserSerializer < ActiveModel::Serializer
 
   def gifts
     self.object.gifts.map do |gift|
+      pge = gift.person_gift_events.map do |pge|
+        {person: pge.person, event: pge.event, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
+      end
       {
         id: gift.id,
         name: gift.name,
@@ -47,6 +57,7 @@ class UserSerializer < ActiveModel::Serializer
         age_range: gift.age_range,
         people: gift.people,
         events: gift.events,
+        person_gift_events: pge,
         pending_events: gift.pending_events,
         pending_people: gift.pending_people
       }
