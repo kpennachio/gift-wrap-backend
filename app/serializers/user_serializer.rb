7f13c358e0b1599c1c4e9fbaca2b70c9ad55fbdd
cannoli9917 <katie.pennachio@gmail.com>
2019-04-06 18:53:34 -0400

    serializer updates

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -6,7 +6,7 @@ class UserSerializer < ActiveModel::Serializer
         date = Date.strptime(event.date, "%m-%d-%Y")
 
         pge = event.person_gift_events.map do |pge|
-          {person: pge.person, gift: pge.gift, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
+          {id: pge.id, person: pge.person, gift: pge.gift, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
         end
       {
         id: event.id,
@@ -19,6 +19,7 @@ class UserSerializer < ActiveModel::Serializer
         notes: event.notes,
         registryLink: event.registry_link,
         person_gift_events: pge,
+        event_gift_ideas: event.event_gift_ideas,
         gift_ideas: event.gift_ideas
       }
     end
@@ -27,7 +28,7 @@ class UserSerializer < ActiveModel::Serializer
   def people
     self.object.people.map do |person|
       pge = person.person_gift_events.map do |pge|
-        {event: pge.event, gift: pge.gift, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
+        {id: pge.id, event: pge.event, gift: pge.gift, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
       end
       {
         id: person.id,
@@ -36,6 +37,7 @@ class UserSerializer < ActiveModel::Serializer
         gifts: person.gifts,
         events: person.events,
         person_gift_events: pge,
+        person_gift_ideas: person.person_gift_ideas,
         gift_ideas: person.gift_ideas
       }
     end
@@ -44,7 +46,7 @@ class UserSerializer < ActiveModel::Serializer
   def gifts
     self.object.gifts.map do |gift|
       pge = gift.person_gift_events.map do |pge|
-        {person: pge.person, event: pge.event, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
+        {id: pge.id, person: pge.person, event: pge.event, gift_actual_cost: pge.gift_actual_cost, price_min: pge.price_min, price_max: pge.price_max}
       end
       {
         id: gift.id,
