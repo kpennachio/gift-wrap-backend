791509f9ddb802c7b95d13b3b1e356df622241bf
cannoli9917 <katie.pennachio@gmail.com>
2019-04-11 11:47:38 -0400

    create user with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   has_many :events
   has_many :budgets
   has_secure_password
+
+  validates :username, :password, :email, :first_name, :last_name, presence: true
+
 end
