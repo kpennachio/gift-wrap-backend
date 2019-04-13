48849e35ced0a16a6846688ab24ec148797f1306
cannoli9917 <katie.pennachio@gmail.com>
2019-04-13 16:42:39 -0400

    welcome email sends

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
+gem 'dotenv-rails'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+       :address => "smtp.gmail.com",
+       :port => 587,
+       :user_name => ENV["USERNAME"],
+       :password => ENV["PASSWORD"],
+       :authentication => :plain,
+       :enable_starttls_auto => true
+     }
+
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
