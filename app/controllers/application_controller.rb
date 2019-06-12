3467b5691fbafc1514b6ab2620b63e129d2792d5
cannoli9917 <katie.pennachio@gmail.com>
2019-06-12 09:40:03 -0400

    clean up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  # -> lets check to see IF we have headers
+  # -> check to see IF we have headers
      # -> is there a token
      # -> decode the token
      # -> send back user id
@@ -7,13 +7,12 @@ class ApplicationController < ActionController::API
  def encode_token(payload)
    JWT.encode(payload, ENV["JWT_KEY"])
    # payload {user_id: 1}
-   # => enaj.fewfwaf.dfadf
+   # => encrypted key
  end
 
  def decode_token
    # if headers -> let's decode it else return nil
    # JWT.decode()
-   # no need to check if there are headers because of the begin rescue!!
    begin
      JWT.decode(auth_headers, ENV["JWT_KEY"])
    rescue
@@ -30,19 +29,17 @@ class ApplicationController < ActionController::API
  def curr_user
    user_id = decode_token[0]["user_id"]
    User.find(user_id)
-   # who is the current user maybe we don't need to authorize the route but it might be nice to know
-   # who the user is to see whether or not they can validly access something
  end
 
- def logged_in
-   # is there a curr_user
-   !!curr_user
- end
-
- def authorized
-   # if there is a curr_user meaning they've been correctly validated
-   # allow them in our application
-   # otherwise send an error
-   render json: {errors: 'Please log in'} unless logged_in
- end
+ # def logged_in
+ #   # is there a curr_user
+ #   !!curr_user
+ # end
+ #
+ # def authorized
+ #   # if there is a curr_user meaning they've been correctly validated
+ #   # allow them in our application
+ #   # otherwise send an error
+ #   render json: {errors: 'Please log in'} unless logged_in
+ # end
 end
